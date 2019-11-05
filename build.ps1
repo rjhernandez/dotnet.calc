84bff491f2d6c5d4aec8bef26e83b31bbc8262ff
Roberto Hernandez <roberto.hernandez@infernored.com>
2019-11-04 21:19:37 -0500

    Fixed paths on .ps1 file.

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
 # My.Calculator (restore, build)
-dotnet restore src/My.Calculator/My.Calculator.Tests.csproj
-dotnet build src/My.Calculator/My.Calculator.Tests.csproj
+dotnet restore src/My.Calculator/My.Calculator.csproj
+dotnet build src/My.Calculator/My.Calculator.csproj
 
 # My.Calculator.Tests (restore, build)
 dotnet restore src/My.Calculator.Tests/My.Calculator.Tests.csproj
